99127a3fbf088c59ca10ae60b642815db3a15429
K07H <K07H@users.noreply.github.com>
2018-05-27 07:54:34 +0200

    Initial commit
    First commit: Preview release!

---
+++ DecorationsFabricator/QPatch.cs
@@ -0,0 +1,33 @@
+﻿namespace DecorationsFabricator
+{
+    using UnityEngine;
+
+    public class QPatch
+    {
+
+        public static void Patch()
+        {
+            // Retrieve original game objects
+            DecorationsFabricatorModule.OriginalToyCarObject = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/Goldglove_car_02");
+            DecorationsFabricatorModule.OriginalPosterObject = Resources.Load<GameObject>("WorldEntities/Environment/Wrecks/Poster");
+            DecorationsFabricatorModule.OriginalPosterAuroraObject = Resources.Load<GameObject>("WorldEntities/Environment/Wrecks/poster_aurora");
+            DecorationsFabricatorModule.OriginalPosterExosuit1Object = Resources.Load<GameObject>("WorldEntities/Environment/Wrecks/poster_exosuit_01");
+            DecorationsFabricatorModule.OriginalPosterExosuit2Object = Resources.Load<GameObject>("WorldEntities/Environment/Wrecks/poster_exosuit_02");
+            DecorationsFabricatorModule.OriginalPosterKittyObject = Resources.Load<GameObject>("WorldEntities/Environment/Wrecks/poster_kitty");
+            DecorationsFabricatorModule.OriginalLabContainerObject = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/biodome_lab_containers_close_01");
+            DecorationsFabricatorModule.OriginalLabContainer2Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/biodome_lab_containers_close_02");
+            DecorationsFabricatorModule.OriginalLabContainer3Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/biodome_lab_containers_tube_01");
+            DecorationsFabricatorModule.OriginalLabContainer4Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/biodome_lab_containers_tube_02");
+            DecorationsFabricatorModule.OriginalLabEquipment1Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/discovery_lab_props_01");
+            DecorationsFabricatorModule.OriginalLabEquipment2Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/discovery_lab_props_02");
+            DecorationsFabricatorModule.OriginalLabEquipment3Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/discovery_lab_props_03");
+            DecorationsFabricatorModule.OriginalCap1Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/descent_plaza_shelf_cap_02");
+            DecorationsFabricatorModule.OriginalCap2Object = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/descent_plaza_shelf_cap_03");
+            DecorationsFabricatorModule.OriginalStarshipSouvenirObject = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/starship_souvenir");
+            DecorationsFabricatorModule.OriginalArcadeGorgetoyObject = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/descent_arcade_gorgetoy_01");
+            DecorationsFabricatorModule.OriginalLuggageBagObject = Resources.Load<GameObject>("WorldEntities/Doodads/Debris/Wrecks/Decoration/docking_luggage_01_bag4");
+
+            DecorationsFabricatorModule.Patch();
+        }
+    }
+}
---
+++ DecorationsFabricator/Utilities.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DecorationsFabricator
+{
+    internal static class Logger
+    {
+        internal static void Log(string text, params object[] args)
+        {
+            if (args != null && args.Length > 0)
+                text = string.Format(text, args);
+            Console.WriteLine($"[DecorationsFabricatorMod] {text}");
+        }
+    }
+
+    public static class RegionAndLanguageHelper
+    {
+        #region Constants
+        private const int GEO_FRIENDLYNAME = 8;
+        #endregion
+
+        #region Private Enums
+        private enum GeoClass : int
+        {
+            Nation = 16,
+            Region = 14
+        };
+        #endregion
+
+        #region Public Enums
+        public enum CountryCode
+        {
+            EN = 0,
+            FR = 1,
+            ES = 2
+        };
+        #endregion
+
+        #region Win32 Declarations
+        [DllImport("kernel32.dll", ExactSpelling = true, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        private static extern int GetUserGeoID(GeoClass geoClass);
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetUserDefaultLCID();
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetGeoInfo(int geoid, int geoType, StringBuilder lpGeoData, int cchData, int langid);
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Returns machine country code.
+        /// </summary>
+        public static CountryCode GetCountryCode()
+        {
+            int geoId = GetUserGeoID(GeoClass.Nation); ;
+            int lcid = GetUserDefaultLCID();
+            StringBuilder locationBuffer = new StringBuilder(100);
+            GetGeoInfo(geoId, GEO_FRIENDLYNAME, locationBuffer, locationBuffer.Capacity, lcid);
+
+            string countryCode = locationBuffer.ToString().Trim();
+
+            if (countryCode.Length > 1)
+            {
+                countryCode = countryCode.Substring(0, 2).ToLowerInvariant();
+                if (countryCode.CompareTo("fr") == 0)
+                    return CountryCode.FR;
+                else if (countryCode.CompareTo("es") == 0)
+                    return CountryCode.ES;
+                else
+                    return CountryCode.EN;
+            }
+            return CountryCode.EN;
+        }
+        #endregion
+    }
+}
