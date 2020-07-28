7edf4c0aa953cd2725ed7235048c85d1525877b5
O-S Marin <K07H@users.noreply.github.com>
2020-07-28 19:38:08 +0200

    Added null check

--- DecorationsMod/Controllers/ReactorLampBrightness.cs
+++ DecorationsMod/Controllers/ReactorLampBrightness.cs
@@ -356,6 +356,8 @@ namespace DecorationsMod.Controllers
                 }
 
                 string rawState = File.ReadAllText(filePath).Replace(',', '.'); // Replace , with . for backward compatibility.
+                if (rawState == null)
+                    return;
                 string[] state = rawState.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
                 if (state != null && state.Length == 8)
                 {
